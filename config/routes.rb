5453771f7f69d652921e67815b697a6d6461af01
mazurkiewicz <thomas.mazurkiewicz.89@gmail.com>
2014-03-12 14:42:49 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Coloclify::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'colocify#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

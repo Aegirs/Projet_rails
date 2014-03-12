96bd41ff68947dbcbfca663946a4481886ecce99
mazurkiewicz <thomas.mazurkiewicz.89@gmail.com>
2014-03-12 20:24:46 +0100

    ok

--- app/models/colocation.rb
+++ app/models/colocation.rb
@@ -4,7 +4,5 @@ class Colocation < ActiveRecord::Base
 	validates :nbmaxcoloc, presence: true
 	validates :adress, presence: true
 
-	/*geocoded_by :adress
-	after_validation :geocode, :if => :adress_changed?*/
-  
+	
 end

511b912c0e54464c6d651a2de2aed1f4a7effc1b
mazurkiewicz <thomas.mazurkiewicz.89@gmail.com>
2014-03-12 14:28:30 +0100

    ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coloclify::Application.config.secret_key_base = 'b731f543ee6cfb646464f67d0c78017b94ee9ea371890c9d0f86bf21b58a51496b1b9135afa64675950930b4456e74a73a5dc39cd1b68b1dacbe3ff720314039'

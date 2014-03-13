3ac6ad65e1f32f6789bedb4b9bd8379794ad7902
mazurkiewicz <thomas.mazurkiewicz.89@gmail.com>
2014-03-13 10:56:44 +0100

    ok

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,9 @@ module Coloclify
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-	config.assets.enabled = true
-	config.assets.prefix = Rails.root.join('tmp/assets').to_s
-	config.assets.compile = true
+	#config.assets.enabled = true
+	#config.assets.prefix = Rails.root.join('tmp/assets').to_s
+	#config.assets.compile = true
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]

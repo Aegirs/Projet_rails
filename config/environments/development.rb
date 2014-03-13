2c0949ec93238b3f3005aefe18f1ac68d3e85af1
mazurkiewicz <thomas.mazurkiewicz.89@gmail.com>
2014-03-13 11:56:09 +0100

    ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,4 @@ Coloclify::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.assets.compress = false
 end

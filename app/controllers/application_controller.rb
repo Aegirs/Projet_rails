0d295f243f798e0420be49efa5759d4e52b6c601
mazurkiewicz <thomas.mazurkiewicz.89@gmail.com>
2014-03-21 14:03:16 +0100

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+  protected
 
+  def configure_permitted_parameters
+    
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :password, :password_confirmation,:email,:pseudo,:nom,:prenom,:ddn,:sexe,:avatar) }
+
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:password,:current_password,:password_confirmation,:email,:pseudo,:nom,:prenom,:ddn,:sexe,:avatar) }
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 
 Coloclify::Application.routes.draw do
 
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users
   devise_for :admins
  
   resources :comptes
   resources :colocations do 
     resources :photos
   end
-  resources :messages
   resources :messages
   resources :acceuil
   root to: "colocations#index"
